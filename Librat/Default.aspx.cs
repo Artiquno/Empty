e71351b9431e2d1cd9bcc814c8dca2369cecf3b4
Artiquno <endri-kurushi@hotmail.com>
2016-12-09 21:05:30 +0100

    Added book repeaters

--- Librat/Default.aspx.cs
+++ Librat/Default.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +10,32 @@ public partial class Librat_Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        List<Book> dataSource = new List<Book>();
 
+        var path = Server.MapPath("~/img/works");
+        var files = Directory.EnumerateFiles(path);
+        string vPath = "/img/works/";
+
+        foreach (var item in files)
+        {
+
+            FileInfo fInfo = new FileInfo(item);
+
+            string url = vPath + fInfo.Name;
+
+            //Note: Take these from a DB
+            dataSource.Add(new Book()
+            {
+                AltText = url,
+                Author = url,
+                Description = "DESCRIPTION: " + url,
+                ImageUrl = url,
+                Title = url,
+                Genre = new List<string>(fInfo.Name.Split(new char[] { '-', '.' }))
+            });
+        }
+
+        this.repeaterBooks.DataSource = dataSource;
+        this.repeaterBooks.DataBind();
     }
 }
