1653eaaa615c8b4eeb7f3ef2aa53b35f80433a9a
Artiquno <endri-kurushi@hotmail.com>
2017-01-13 00:12:53 +0100

    Because there's nothing better than taking mail from your alter ego

---
+++ Demos/Mail.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Mail.aspx.cs" Inherits="Demos_Mail" %>
+<%@ Register TagPrefix="en3" TagName="ContactForm" Src="~/Controls/ContactForm.ascx" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Mailing stuff</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        Message Sent
+    </div>
+    </form>
+</body>
+</html>
---
+++ Demos/Mail.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Mail;
+
+public partial class Demos_Mail : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        MailMessage mail = new MailMessage("libraria_error_log@hotmail.com", "endri-kurushi@hotmail.com");
+        mail.Subject = "Just Testing";
+        mail.Body = "Just testing mailing in ASP.NET so don't mind me.";
+
+        SmtpClient smtp = new SmtpClient();
+        smtp.Send(mail);
+    }
+}
